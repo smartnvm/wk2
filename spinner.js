11dfa40a0850bc4771476cc8d97531a786645f39
AJ <aj@smartnvm.com>
2021-09-22 04:32:04 +0000

    spinner animation

---
+++ spinner.js
@@ -0,0 +1,61 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+const spinner = () => {
+  let key = 'a'
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, 100);
+
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, 300);
+
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, 500);
+
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   ');
+  }, 700);
+
+  setTimeout(() => {
+    process.stdout.write('\r\|   ');
+  }, 900);
+
+
+  setTimeout(() => {
+    process.stdout.write('\r\/   ');
+  }, 1100);
+
+  setTimeout(() => {
+    process.stdout.write('\r\-   ');
+  }, 1100);
+
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   ');
+  }, 1300);
+
+
+  setTimeout(() => {
+    process.stdout.write('\r\|   ');
+    key = 'b'
+  }, 1500);
+
+
+}
+
+
+// return key
+
+// x = spinner()
+
+
+// setTimeout(() => {
+//   console.log(x)
+//   process.stdout.write('\n');
+//   console.log(x)
+
+// }, 1800);
+
